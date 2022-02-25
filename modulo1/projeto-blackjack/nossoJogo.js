376848bc476842b4ffee564f3adee8eb54bbd83d
EduardaPmeireles <contatoeduardapmeireles@hotmail.com>
2022-02-25 16:30:53 -0300

    Adiciona a resolução do projeto black-jack

---
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -0,0 +1,38 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+console.log("Bem-vindo ao jogo de Blackjack!");
+
+   if(confirm("Quer iniciar uma nova rodada?")) {
+
+      const cartasUsuario = [comprarCarta(), comprarCarta()];
+      const cartasComputador = [comprarCarta(), comprarCarta()];
+
+      const somaCartasUsuario = cartasUsuario[0].valor + cartasUsuario[1].valor;
+      const somaCartasComputador = cartasComputador[0].valor + cartasComputador[1].valor;
+
+      console.log(`Usuário - cartas: ${cartasUsuario[0].texto} ${cartasUsuario[1].texto} - pontuação ${somaCartasUsuario}`);
+      console.log(`Computador - cartas: ${cartasComputador[0].texto} ${cartasComputador[1].texto} - pontuação ${somaCartasComputador}` );
+
+         if(somaCartasComputador === somaCartasUsuario) {
+            console.log(`Empate!`);
+         }
+         else if (somaCartasComputador > somaCartasUsuario) {
+            console.log(`O computador venceu!`);
+         }
+         else if (somaCartasComputador < somaCartasUsuario) {
+            console.log(`O usuário venceu!`)
+         };
+
+   } else {
+      console.log("O jogo acabou.");
+};
