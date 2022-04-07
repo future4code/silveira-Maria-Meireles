fb4758fd45cb3964186a7ec898ab98083265a874
EduardaPmeireles <contatoeduardapmeireles@hotmail.com>
2022-04-07 16:56:13 -0300

    iniciando projeto

---
+++ modulo2/projeto-labefy/labefy/src/pages/ListaPlaylists.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class ListaPlaylists extends React.Component{
+
+    render() {
+
+        return (
+            <p> coé </p>
+        )
+    }
+}
---
+++ modulo2/projeto-labefy/labefy/src/pages/PaginaHome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class PaginaHome extends React.Component{
+
+    render () {
+
+        return (
+            <p> AlÔ </p>
+        )
+    }
+}
