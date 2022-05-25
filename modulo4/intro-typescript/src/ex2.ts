e8140354761ca4b1f64ceecbf5a8fb20932f400e
EduardaPmeireles <contatoeduardapmeireles@hotmail.com>
2022-05-25 17:09:28 -0300

    Resolução da intro ao typescript

---
+++ modulo4/intro-typescript/src/ex2.ts
@@ -0,0 +1,10 @@
+const imprimeCoresFavoritas = ():void => {
+    const cor1: string = 'preto'
+    const cor2: string = 'vermelho'
+    const cor3: string = 'azul'
+
+    console.log([cor1, cor2, cor3])
+}
+
+imprimeCoresFavoritas()
+// Minha instalação do process.argv não rolou, tive de colocar os dados mockados :(
